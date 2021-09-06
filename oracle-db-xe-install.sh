b111d709af453abe906d9a90b9992a364c5313f3
Charles Byrd <cbyrd@cbyrd-linux-wk.mgsddc.lab>
2021-09-06 08:10:44 -0400

    script update

--- oracle-db-xe-install.sh
+++ oracle-db-xe-install.sh
@@ -5,14 +5,17 @@ curl -o oracle-database-preinstall-18c-1.0-1.el7.x86_64.rpm https://yum.oracle.c
 # Install preinstall RPM
 yum -y localinstall oracle-database-preinstall-18c-1.0-1.el7.x86_64.rpm > /xe_logs/XEsilentinstall.log 2>&1
 
+# Install wget
+yum install -y wget
+
 # Download oracle XE install  RPM
 wget https://download.oracle.com/otn-pub/otn_software/db-express/oracle-database-xe-18c-1.0-1.x86_64.rpm
 # Install oracle XE install RPM
 yum -y localinstall oracle-database-xe-18c-1.0-1.x86_64.rpm > /xe_logs/XEsilentinstall.log 2>&1
 
 # Remove RPM files
-rm oracle-database-preinstall-18c-1.0-1.el7.x86_64.rpm
-rm oracle-database-xe-18c-1.0-1.x86_64.rpm
+rm -rf oracle-database-preinstall-18c-1.0-1.el7.x86_64.rpm
+rm -rf oracle-database-xe-18c-1.0-1.x86_64.rpm
 
 # Configure XE DB
 (echo "password"; echo "password";) | /etc/init.d/oracle-xe-18c configure >> /xe_logs/XEsilentinstall.log 2>&1
